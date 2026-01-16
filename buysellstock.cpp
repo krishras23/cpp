0318f79757d04cf94924f09b88469a07967f6a75
Krish Rastogi <krishras23@gmail.com>
2026-01-16 10:46:04 -0500

    buy sell stock

---
+++ buysellstock.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+   int maxProfit(vector<int> &prices){
+      int minPrice = INT_MAX;
+      int maxProfit = 0;
+
+      for (int price : prices){
+         minPrice = min(minPrice, price);
+         maxProfit = max(maxProfit, price - minPrice);
+      }
+
+      return maxProfit;
+   }
+};
